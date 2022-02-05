36f0caee11e6a2318f1cdfed4a530f48db42a766
Patrick Tavares <patricktavares3pc@gmail.com>
2022-02-05 09:36:55 -0300

    Incluindo um container para ter demarcação e centralizar o conteudo

---
+++ src/AppStyle.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+        max-width:1440px;
+        margin: auto;
+        background-color:#E5E5E5;
+        height:100vh;
+        display:block;
+
+        .cont-initial{
+                width: 100%;
+                display: flex;
+                justify-content:center;
+                flex-direction:column;
+                gap:50px;
+        }
+        .image-phone{
+                padding-top: 30px;
+                width: 300px;
+                margin: auto;
+        }
+        button{
+                margin: auto;
+                background-color: #F2994A;
+                color:#FFFFFF ;
+
+        }
+`
+
---
+++ src/components/Container/Container.js
@@ -0,0 +1,10 @@
+import CContainer from "./style"
+
+
+const Container = () => {
+    return (
+        <CContainer />
+    )
+}
+
+export default Container 
